fb80341f53108d078391f657e4980369808a172e
Demi [Alvaro Martinez de Miguel] <demipel8@gmail.com>
2017-07-24 15:51:45 +0200

    Create options page

---
+++ client/options.js
@@ -0,0 +1,24 @@
+const TAG = 'tag'
+const defaultTag = 'ok'
+const getElement = document.getElementById.bind(document)
+
+document.addEventListener('DOMContentLoaded', restore_options);
+getElement('save').addEventListener('click', save_options);
+
+function save_options() {
+  let tag = getElement(TAG).value
+
+  chrome.storage.sync.set({ tag }, function() {
+    let status = getElement('status');
+    status.textContent = 'Options saved.';
+    setTimeout(function() {
+      status.textContent = '';
+    }, 750);
+  });
+}
+
+function restore_options() {
+  chrome.storage.sync.get({ tag: defaultTag }, items => {
+    getElement(TAG).value = items.tag;
+  });
+}
